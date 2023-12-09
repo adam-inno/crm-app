b04892c92ca58ed36476fe35768d3659302aee09
adamz <58021562+ad4mny@users.noreply.github.com>
2023-12-09 22:43:26 +0800

    protect navigaiton routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        @if(Auth::check())
                         @include('layouts.navigation')
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
